7f76aa89ebc6106469895d48a6d093e06c03d45d
李晋 <lijin@chanjet.com>
2016-07-03 14:16:27 +0800

    update

--- src/Migrator.Tests/Data/TestMigrations.cs
+++ src/Migrator.Tests/Data/TestMigrations.cs
@@ -1,68 +1,102 @@
+using System.Data;
 using Migrator.Framework;
 
 namespace Migrator.Tests.Data
 {
-    [Migration(1)]
-    public class FirstTestMigration : Migration
-    {
-        override public void Up()
-        {
-        }
-        override public void Down()
-        {
-        }
-    }
+	[Migration(1)]
+	public class FirstTestMigration : Migration
+	{
+		override public void Up()
+		{
+		}
+		override public void Down()
+		{
+		}
+	}
 
-    [Migration(2)]
-    public class SecondTestMigration : IMigration
-    {
-        private ITransformationProvider _transformationProvider;
+	[Migration(2)]
+	public class SecondTestMigration : IMigration
+	{
+		private ITransformationProvider _transformationProvider;
 
-        public string Name
-        {
-            get { return StringUtils.ToHumanName(GetType().Name); }
-        }
+		public string Name
+		{
+			get { return StringUtils.ToHumanName(GetType().Name); }
+		}
 
-        /// <summary>
-        /// Defines tranformations to port the database to the current version.
-        /// </summary>
-        public void Up() {}
+		/// <summary>
+		/// Defines tranformations to port the database to the current version.
+		/// </summary>
+		public void Up() {}
 
-        /// <summary>
-        /// This is run after the Up transaction has been committed
-        /// </summary>
-        public virtual void AfterUp()
-        {
-        }
+		/// <summary>
+		/// This is run after the Up transaction has been committed
+		/// </summary>
+		public virtual void AfterUp()
+		{
+		}
 
-        /// <summary>
-        /// Defines transformations to revert things done in <c>Up</c>.
-        /// </summary>
-        public void Down() {}
+		/// <summary>
+		/// Defines transformations to revert things done in <c>Up</c>.
+		/// </summary>
+		public void Down() {}
 
-        /// <summary>
-        /// This is run after the Down transaction has been committed
-        /// </summary>
-        public virtual void AfterDown()
-        {
-        }
+		/// <summary>
+		/// This is run after the Down transaction has been committed
+		/// </summary>
+		public virtual void AfterDown()
+		{
+		}
 
-        /// <summary>
-        /// Represents the database.
-        /// <see cref="ITransformationProvider"></see>.
-        /// </summary>
-        /// <seealso cref="ITransformationProvider">Migration.Framework.ITransformationProvider</seealso>
-        public ITransformationProvider Database
-        {
-            get { return _transformationProvider; }
-            set { _transformationProvider = value; }
-        }
+		/// <summary>
+		/// Represents the database.
+		/// <see cref="ITransformationProvider"></see>.
+		/// </summary>
+		/// <seealso cref="ITransformationProvider">Migration.Framework.ITransformationProvider</seealso>
+		public ITransformationProvider Database
+		{
+			get { return _transformationProvider; }
+			set { _transformationProvider = value; }
+		}
 
-        /// <summary>
-        /// This gets called once on the first migration object.
-        /// </summary>
-        public virtual void InitializeOnce(string[] args)
-        {
-        }
-    }
+		/// <summary>
+		/// This gets called once on the first migration object.
+		/// </summary>
+		public virtual void InitializeOnce(string[] args)
+		{
+		}
+	}
+
+	[Migration(20080805151232)]
+	public class Acc20160702154759 : Migration
+	{
+		private const string TableName = "acc";
+
+		public override void Up()
+		{
+			Database.AddTable(TableName,
+//				new Column("acc_id", DbType.String, ColumnProperty.PrimaryKey | ColumnProperty.Unique | ColumnProperty.NotNull | ColumnProperty.Indexed),
+				new Column("acc_id", DbType.String, ColumnProperty.PrimaryKey | ColumnProperty.Unique | ColumnProperty.NotNull),
+				//				new Column("acc_id", DbType.String, ColumnProperty.NotNull),
+				new Column("book_id", DbType.String, ColumnProperty.Null),
+				new Column("name", DbType.String, ColumnProperty.Null)
+				);
+
+			//			Database.Commit();
+
+			//			string[] pks = {"acc_id"};
+			//			Database.AddPrimaryKey(String.Format("PK_{0}", TableName), TableName, pks);
+
+			//			var connectionString = "Data Source=tax_data.db;Version=3";
+			//			var provider = ProviderFactory.Create("SQLite", connectionString);
+			//			provider.AddPrimaryKey(String.Format("PK_{0}", TableName), TableName, pks);
+
+			//			Database.AddColumn(TableName,"acc_id",DbType.Single,ColumnProperty.PrimaryKey | ColumnProperty.NotNull);
+		}
+
+		public override void Down()
+		{
+			Database.RemoveTable(TableName);
+		}
+	}
 }
---
+++ src/Migrator.Tests/TaxMigTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Migrator.Tests.Data;
+using NUnit.Framework;
+
+namespace Migrator.Tests
+{
+	[TestFixture]
+	class TaxMigTests
+	{
+		private Migrator _migrator;
+
+		[SetUp]
+		public void SetUp()
+		{
+			var provider = "SQLite";
+			var connectionString = "Data Source=tax_data.db;Version=3";
+			var asm = Assembly.GetExecutingAssembly();
+			_migrator = new Migrator(provider, connectionString, asm, true);
+
+		}
+
+		[Test]
+		public void MigTest()
+		{
+			_migrator.MigrationsTypes.Clear();
+			var tp = typeof(Acc20160702154759);
+			_migrator.MigrationsTypes.Add(tp);
+			var ver = MigrationLoader.GetMigrationVersion(tp);
+
+			//migrator.MigrateToLastVersion();
+			_migrator.MigrateTo(ver);
+
+			Console.WriteLine();
+
+			Assert.IsTrue(1.Equals(1));
+			
+		}
+	}
+}
